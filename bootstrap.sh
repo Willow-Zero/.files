48429bf1487b49480ef72cea4ee9c6e4fb67dd43
Willow-Zero <zmckinnis4@gmail.com>
2024-10-29 14:13:38 -0400

    look im trying here

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ git clone https://github.com/Willow-Zero/.files.git
 cd .files
 cp .bashrc ~/.bashrc
 sysupdate
-yay -Syu kitty nano neofetch fortune-mod cowsay cmus ranger firefox pidgin finch steam thunderbird gcc aseprite atlauncher-bin audacity bandcamp-dl-git bluez-utils curl desmume dolphin-emu-git discordo-git flashpoint-bin ftb-app-bin gnome-disk-utility graphviz higan-git hyprland hyprpaper 7-zip lutris lynx mgba-sdl minecraft-launcher multimc-bin neofetch network-manager-applet vi waybar yt-dlp 
+yay -Syu kitty nano neofetch fortune-mod cowsay cmus ranger firefox pidgin finch steam thunderbird gcc aseprite atlauncher-bin audacity bandcamp-dl-git bluez-utils curl desmume dolphin-emu-git discordo-git flashpoint-bin ftb-app-bin gnome-disk-utility graphviz higan-git hyprland hyprpaper 7-zip lutris lynx mgba-sdl minecraft-launcher multimc-bin neofetch network-manager-applet vi waybar yt-dlp code 
 pls systemctl enable bluetooth.service
 pls systemctl start bluetooth.service
 pls systemctl enable NetworkManager.service 
